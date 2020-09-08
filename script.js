4c0f32f48118f69fe9ebf22c0a1bb16a7714b8e1
Van <vango85@gmail.com>
2020-09-08 20:52:03 +0200

    [xx] Update for Fall2020

--- script.js
+++ script.js
@@ -1,38 +1,24 @@
-// 1b. Create a function which doubles the age and returns it
-const doubleAge = (age) => {
-    // Function body
+const askAge = () => {
+  const age = prompt("What is your age?");
+  console.log(age);
+  console.log(age * 2);
+};
 
-    // 1a. We'll double the age and console.log it
-    const doubledAge = +age * 2;
-    return doubledAge;
+const doubleAge = (age) => {
+  const doubledAge = age * 2;
+  return doubledAge;
 };
 
 const tripleAge = (age) => {
-    // Function body
-    const tripledAge = +age * 3;
-    return tripledAge;
-}
+  const tripledAge = age * 3;
+  return tripledAge;
+};
 
-const clicked = () => {
-    // 1. Use Prompt/user input
-    const userAge = prompt("What is your age?");
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const age = document.getElementById("age").value;
 
-    // Check if it is odd or even
-
-    // Initialize the variable to the original age
-    let modifiedAged = userAge;
-    console.log(`Original Age: ${userAge}`);
-
-    if (+userAge % 2 == 0) {
-        // a. If even, double the original age
-        modifiedAged = doubleAge(userAge);
-    } else {
-        // b. If odd, triple the age
-        modifiedAged = tripleAge(userAge);
-    }
-    console.log(`Modified Age: ${modifiedAged}`);
-
-    // Print the return value
-    document.getElementById("age").innerHTML = modifiedAged;
-
-}
+  // Print the return value
+  document.getElementById("doubledAge").innerHTML = doubleAge(age);
+  document.getElementById("tripledAge").innerHTML = tripleAge(age);
+};
